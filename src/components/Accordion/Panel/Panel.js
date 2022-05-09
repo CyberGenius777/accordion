d4dcc50b50a75973eec55cf60b5f32191d96d5fd
Гаджиев Эльдар Ражидинович <ERGadzhiev@sberbank.ru>
2022-05-10 02:31:32 +0300

    panel changes

--- src/components/Accordion/Panel/Panel.js
+++ src/components/Accordion/Panel/Panel.js
@@ -1,25 +1,19 @@
-import React, { useRef } from 'react'
+import React from 'react'
 import { ObjectInfo, PanelContainer, Title } from './styles'
 
 const Panel = ({ children, index, activeKey, setActiveKey, ...rest }) => {
-  const ref = useRef(null)
-  console.log('ref', ref.current ? ref.current.offsetHeight : null)
-  const onPanelClick = (e, idx) => {
-    if (activeKey === idx) {
+  const onPanelClick = () => {
+    if (activeKey === index) {
       setActiveKey(null)
     } else {
-      setActiveKey(idx)
+      setActiveKey(index)
     }
   }
 
   return (
-    <PanelContainer
-      {...rest}
-      // objectRef={ref ? ref.current.offsetHeight : null}
-      key={index}
-      isActive={activeKey === index}>
+    <PanelContainer {...rest} onClick={onPanelClick} key={index} isActive={activeKey === index}>
       <Title>{children.props.header}</Title>
-      {activeKey === index ? <ObjectInfo ref={ref}>{children.props.children}</ObjectInfo> : null}
+      {activeKey === index ? <ObjectInfo>{children.props.children}</ObjectInfo> : null}
     </PanelContainer>
   )
 }
